2b26afc1c2d280f040073e07ead5998d263e4e5b
Eraldo Jr <esilvaju@cern.ch>
2019-07-10 15:05:50 +0200

    fix module

--- show-lc.sh
+++ show-lc.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
 
-echo "uninstalling SIMPLE GRID module"
-puppet module uninstall --ignore-changes maany-simple_grid
-
-echo "Removing etc simple_grid dir"
-rm -rf /etc/simple_grid/
-
-echo "Leaving swarm network"
-docker swarm leave --force
-[root@h-lc-01 helper]# cat show_simple.sh 
-#/bin/sh
-
 echo -e "\n"
 echo  "---------"
 echo  ".stage"
