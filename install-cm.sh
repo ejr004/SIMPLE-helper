ae70f3d55b229eb44c81d6f3e968619f73620baa
Eraldo Jr <esilvaju@cern.ch>
2019-07-10 15:11:02 +0200

    fix install-cm

--- install-cm.sh
+++ install-cm.sh
@@ -7,8 +7,11 @@ echo "Creating sample site-level-config file and dir"
 puppet apply -e 'class{"simple_grid::install::config_master::simple_installer::create_sample_site_level_config_file":}'
 
 read -p "XXXX Press enter to continue: XXXX"
+
 echo "Coping site_level_config_file.yaml"
 cp /root/site-level-config.yaml /etc/simple_grid/site_config/
+
+echo "Coping dummy scripts"
 cp -r /root/SIMPLE-helper/lifecycle/ /etc/simple_grid/
 
 echo "Running SIMPLE GRID install stage"
