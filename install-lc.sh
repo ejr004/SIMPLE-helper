2540466ddf8f470b7f64a67cc99a96be0a136e22
Eraldo Jr <esilvaju@cern.ch>
2019-07-10 14:13:16 +0200

    fix puppet apply

--- install-lc.sh
+++ install-lc.sh
@@ -3,16 +3,23 @@
 echo "Installing SIMPLE GRID module"
 puppet module install --module_repository=http://lc-02.cern.ch maany-simple_grid
 
-echo "Running SIMPLE GRID installer for LC"
+echo "Running SIMPLE GRID installer for LC
 puppet apply --modulepath /etc/puppetlabs/code/environments/production/modules -e "class {'simple_grid::install::lightweight_component::simple_installer':puppet_master => 'h-cm-01.cern.ch'}"
+"
+
+
 
 
 echo "Now sign the certificates on CM \n
 XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+ Step 1.
       Add to /etc/puppetlabs/puppet/puppet.conf
 
       [main]
       module_repository = http://lc-02.cern.ch
+ Step 2.
+ puppet apply --modulepath /etc/puppetlabs/code/environments/production/modules -e "class {'simple_grid::install::lightweight_component::simple_installer':puppet_master => 'basic_config_master.cern.ch'}"
 
+Step 3.
       On CM:
       puppet cert sign --all"
