18a3143559ab6db3857abbaf777ab2cd6eb41458
Eraldo Jr <esilvaju@cern.ch>
2019-07-10 12:48:57 +0200

    adding dumb lifecycle scripts

--- cern-base-cm.sh
+++ cern-base-cm.sh
@@ -8,11 +8,16 @@ systemctl stop firewalld
 sed -i 's/enforcing/disabled/' /etc/selinux/config
 
 # Remove cern puppet
-rpm -e puppet*
+yum remove puppet* -y
 
 # Install puppet
 rpm -Uvh http://yum.puppet.com/puppet5-release-el-7.noarch.rpm
 yum install -y --disablerepo=cern puppetserver
 yum install -y --disablerepo=cern puppet-agent
 
+echo "Puppet server version should be  5.3.8"
+puppetserver --version
+echo "Puppet client version should be 5.5.14"
+puppet --version
+# Install vim
 yum install -y vim-enhanced 
