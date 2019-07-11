0f148873e374aa65bfd4d8e12cd03491aac56c28
root <root@h-lc-01.cern.ch>
2019-07-11 17:30:09 +0200

    Fix install-lc

--- install-lc.sh
+++ install-lc.sh
@@ -12,6 +12,6 @@ sed -i "1i[main]" /etc/puppetlabs/puppet/puppet.conf
 read -p "Enter with the Puppet Server fqdn and press enter:   "  cm_fqdn
 echo "Running puppet apply with $cm_fqdn as puppet server"
 
-puppet apply --modulepath /etc/puppetlabs/code/environments/production/modules -e "class {'simple_grid::install::lightweight_component::simple_installer':puppet_master => '$cm_qdn'}"
+puppet apply --modulepath /etc/puppetlabs/code/environments/production/modules -e "class {'simple_grid::install::lightweight_component::simple_installer':puppet_master => '$cm_fqdn'}"
 
 echo -e "Now sign the certificates for the CM: \n puppet cert sign --all"
